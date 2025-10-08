9565e4c02de7ff862cfe5fbc07ae66c32e9b54bc
nikeshskillup <142885550+nikeshskillup@users.noreply.github.com>
2025-10-08 09:23:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   const [showProductList, setShowProductList] = useState(false);
 
-  const handleGetStartedClick = () => {
+  const handleStartedClick = () => {
     setShowProductList(true);
   };
 
@@ -18,12 +18,12 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
+            
           </button>
          </div>
           <div className="aboutus_container">
@@ -33,7 +33,7 @@ function App() {
 
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
+        
       </div>
     </div>
   );
